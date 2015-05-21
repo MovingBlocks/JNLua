f3242809bea90c7eef90f9e3de11adb8279288b1
Florian Nücke <florian@nuecke.de>
2015-05-21 14:39:03 +0200

    Avoid NPE in argument error generation when there is no such argument.

--- src/main/java/org/terasology/jnlua/LuaState.java
+++ src/main/java/org/terasology/jnlua/LuaState.java
@@ -2272,9 +2272,10 @@ public class LuaState {
 	 * Creates a Lua runtime exception to indicate an argument type error.
 	 */
 	private LuaRuntimeException getArgTypeException(int index, LuaType type) {
+		final LuaType have = type(index);
 		return getArgException(index,
 				String.format("%s expected, got %s", type.toString()
-						.toLowerCase(), type(index).toString().toLowerCase()));
+						.toLowerCase(), have != null ? have.toString().toLowerCase() : "[type unknown, out of bounds]"));
 	}
 
 	/**
